8f9d8a6c23d4e26cd7edc3f26a3752d6b6badacd
markedwardmurray <markedwardmurray@gmail.com>
2015-12-10 11:47:56 -0500

    layout & outlet/action

--- WowSuchHire/ViewController.swift
+++ WowSuchHire/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var inputField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("dogeCell", forIndexPath: indexPath)
+        
+        return cell
+    }
+    
+    @IBAction func saveTapped(sender: AnyObject) {
+    }
+    
 }
 
