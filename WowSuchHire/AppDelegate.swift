157d50dc2b7443d9bb640ad429d672a2ee33d8d0
markedwardmurray <markedwardmurray@gmail.com>
2015-12-10 11:50:59 -0500

    enable IQKeyboardManager

--- WowSuchHire/AppDelegate.swift
+++ WowSuchHire/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Parse
 import Bolts
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        IQKeyboardManager.sharedManager().enable = true
+        
         Parse.enableLocalDatastore()
         
         Parse.setApplicationId(applicationID, clientKey: clientKey)
